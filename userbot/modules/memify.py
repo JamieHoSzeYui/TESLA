50860df99308f4a24572ef11603788d79ba2266f
JamieHoSzeYui <wileylau@gmail.com>
2020-09-04 12:04:35 +0800

    Memeify Fixup

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -8,8 +8,7 @@ import os
 from userbot.events import register
 from userbot import CMD_HELP
 
-
-@register("\.memify ?(.*)")
+@register(outgoing=True, pattern="^.memify$)
 async def handler(event):
     if event.fwd_from:
         return
