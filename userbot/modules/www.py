30977677d5177ca9d75f2f890062df6b2eed0b09
JamieHoSzeYui <wileylau@gmail.com>
2020-09-04 14:22:00 +0800

    Drop speedtest

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -13,46 +13,6 @@ from telethon import functions
 from userbot import CMD_HELP
 from userbot.events import register
 
-
-@register(outgoing=True, pattern="^.speed$")
-async def speedtst(spd):
-    """ For .speed command, use SpeedTest to check server speeds. """
-    await spd.edit("`Running speed test . . .`")
-    test = Speedtest()
-
-    test.get_best_server()
-    test.download()
-    test.upload()
-    test.results.share()
-    result = test.results.dict()
-
-    await spd.edit("`"
-                   "Started at "
-                   f"{result['timestamp']} \n\n"
-                   "Download "
-                   f"{speed_convert(result['download'])} \n"
-                   "Upload "
-                   f"{speed_convert(result['upload'])} \n"
-                   "Ping "
-                   f"{result['ping']} \n"
-                   "ISP "
-                   f"{result['client']['isp']}"
-                   "`")
-
-
-def speed_convert(size):
-    """
-    Hi human, you can't read bytes?
-    """
-    power = 2**10
-    zero = 0
-    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
-    while size > power:
-        size /= power
-        zero += 1
-    return f"{round(size, 2)} {units[zero]}"
-
-
 @register(outgoing=True, pattern="^.dc$")
 async def neardc(event):
     """ For .dc command, get the nearest datacenter information. """
@@ -72,9 +32,6 @@ async def pingme(pong):
     await pong.edit("`Pong!\n%sms`" % (duration))
 
 
-CMD_HELP.update(
-    {"speed": ".speed\
-    \nUsage: Does a speedtest and shows the results."})
 CMD_HELP.update(
     {"dc": ".dc\
     \nUsage: Finds the nearest datacenter from your server."})
