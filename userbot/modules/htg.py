5e3cf4678707a14218b24f9e412f641a3730f714
henloboi <henloboii@protonmail.ch>
2020-09-05 11:26:05 +0800

    Syntax fixes

--- userbot/modules/htg.py
+++ userbot/modules/htg.py
@@ -7,21 +7,19 @@
 from userbot.events import register
 from userbot import CMD_HELP
 
-
 @register(outgoing=True, pattern=r"\.htg ?(.*)")
 async def handler(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
     query = input_str.replace(" ", "+")
-    url = f"https://lmgtfy.com/?q={query}"
+    url = f"https://lmgtfy.com/?q={query}&iie=1"
     try:
-       webpage = requests.get(url).text
-       if webpage:
-           await event.edit(f"More info about \"[{input_str}]({url})\"")
-   except:
-       await event.edit(f"More info about \"[{input_str}]({url})\"")
-
+        webpage = requests.get(url).text
+        if webpage:
+            await event.edit(f"More info about \"[{input_str}]({url})\"")
+    except:
+        await event.edit(f"More info about \"[{input_str}]({url})\"")
 
 
 CMD_HELP.update({"htg": "\
