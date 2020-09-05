ec26507319a8bad7990df9afd70380a8884615a0
ElytrA8 <manofuranium@gmail.com>
2020-09-05 03:38:16 +0000

    pylint: auto fixes

--- userbot/modules/clone.py
+++ userbot/modules/clone.py
@@ -8,7 +8,7 @@ from telethon.tl.types import InputPhoto
 from userbot.events import register
 from userbot import CMD_HELP
 
-    userObj = False
+userObj = False
 
 
 @register(outgoing=True, pattern=r"\.memify ?(.*)")
@@ -55,7 +55,7 @@ async def updateProfile(userObj, reset=False):
             userPfp = userObj.profile_photo
             pfpImage = await client.download_media(userPfp)
             await client(UploadProfilePhotoRequest(await client.upload_file(pfpImage)))
-        except:
+        except BaseException:
             pass
     await client(UpdateProfileRequest(
         about=userAbout, first_name=firstName, last_name=lastName
